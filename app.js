3626907629ae42ffe82d95f5f298170c13bb0690
alibi <tolebayalibi24@gmail.com>
2021-11-07 02:31:55 +0600

    commit

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ var MySQLStore = require('express-mysql-session')(session);
 
 var sessionStore = new MySQLStore({}, connection);
 
-function handleConnection() {
-  var connection = mysql.createConnection({
-    host     : 'us-cdbr-east-04.cleardb.com',  // us-cdbr-east-04.cleardb.com
-    user     : 'b4cd7d6552f2a8',  // b4cd7d6552f2a8
-    password : '3f933e43',  // 3f933e43
-    database : 'heroku_3b662f0c2000bbd'  // heroku_3b662f0c2000bbd
-  });
+var connection = mysql.createConnection({
+  host     : 'us-cdbr-east-04.cleardb.com',  // us-cdbr-east-04.cleardb.com
+  user     : 'b4cd7d6552f2a8',  // b4cd7d6552f2a8
+  password : '3f933e43',  // 3f933e43
+  database : 'heroku_3b662f0c2000bbd'  // heroku_3b662f0c2000bbd
+});
 
+function handleConnection() {
   connection.connect();
 
   connection.on('error', function(err) {
