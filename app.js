480c5e8ce8bf52b1097e6af499a44ae6123c3187
alibi <tolebayalibi24@gmail.com>
2021-11-07 02:47:37 +0600

    commit

--- app.js
+++ app.js
@@ -8,37 +8,20 @@ var app = express();
 var mysql = require('mysql');
 var MySQLStore = require('express-mysql-session')(session);
 
+
+var connection = mysql.createConnection({
+  host: 'us-cdbr-east-04.cleardb.com', // us-cdbr-east-04.cleardb.com
+  user: 'b4cd7d6552f2a8', // b4cd7d6552f2a8
+  password: '3f933e43', // 3f933e43
+  database: 'heroku_3b662f0c2000bbd' // heroku_3b662f0c2000bbd
+});
+
+
 var sessionStore = new MySQLStore({}, connection);
 
 
-var connection;
 
-
-function handleConnection() {
-  connection = mysql.createConnection({
-    host     : 'us-cdbr-east-04.cleardb.com',  // us-cdbr-east-04.cleardb.com
-    user     : 'b4cd7d6552f2a8',  // b4cd7d6552f2a8
-    password : '3f933e43',  // 3f933e43
-    database : 'heroku_3b662f0c2000bbd'  // heroku_3b662f0c2000bbd
-  });
-  connection.connect(function (err) {
-    if (err) {
-      console.log('error when connecting to db: ', err);
-      setTimeout(handleConnection, 2000);
-    }
-  });
-
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-      handleConnection();
-    } else {
-      throw err;
-    }
-  });
-}
-
-handleConnection();
+connection.connect();
  
 global.db = connection;
 
